7dfa9b7aa79d983f2aee3a882f44fec47ad7b887
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-11 23:11:09 +0900

    feat(InputView): 메뉴의 입력 형식을 검사하는 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,18 +1,41 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     public Integer getDate() {
         String input = Console.readLine();
-        validate(input);
+        validateNullOrEmpty(input);
+        validateParsible(input);
         return Integer.parseInt(input);
     }
 
-    private void validate(String input) {
+    public String getMenu() {
+        String input = Console.readLine();
         validateNullOrEmpty(input);
-        validateParsible(input);
+        validateFormatOfMenu(input);
+        return input;
     }
+
+    private void validateFormatOfMenu(String input) {
+        List<String> menus = Arrays.stream(input.split(",")).toList();
+        for (String menu : menus) {
+            validateNullOrEmpty(menu);
+            validateDelimiterOfMenu(menu);
+        }
+    }
+
+    private void validateDelimiterOfMenu(String menu) {
+        menu = menu.trim();
+        int numberOfDash = menu.length() - menu.replace("-", "").length();
+        int index = menu.indexOf('-');
+        if (index == menu.length() - 1 || index == 0 || numberOfDash != 1) {
+            throw new IllegalArgumentException("입력 형식에 맞춰서 입력해주세요");
+        }
+    }
+
 
     private void validateParsible(String input) {
         try {
