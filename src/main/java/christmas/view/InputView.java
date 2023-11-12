5561f1354eeee124faa73fb36590442b3a4bffbc
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-12 18:02:08 +0900

    refactor(InputView): 에러메시지 상수화

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,28 +1,32 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.constants.messages.Error;
 import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
     public Integer getDate() {
         String input = Console.readLine();
-        validateNullOrEmpty(input);
-        validateParsible(input);
+        validateNullOrEmpty(input, Error.DATE.getMessage());
+        validateParsible(input, Error.DATE.getMessage());
         return Integer.parseInt(input);
     }
 
     public String getMenu() {
         String input = Console.readLine();
-        validateNullOrEmpty(input);
+        validateNullOrEmpty(input, Error.MENU.getMessage());
         validateFormatOfMenu(input);
         return input;
     }
 
     private void validateFormatOfMenu(String input) {
         List<String> menus = Arrays.stream(input.split(",")).toList();
+        if (menus.isEmpty()) {
+            throw new IllegalArgumentException(Error.MENU.getMessage());
+        }
         for (String menu : menus) {
-            validateNullOrEmpty(menu);
+            validateNullOrEmpty(menu, Error.MENU.getMessage());
             validateDelimiterOfMenu(menu);
         }
     }
@@ -32,23 +36,23 @@ public class InputView {
         int numberOfDash = menu.length() - menu.replace("-", "").length();
         int index = menu.indexOf('-');
         if (index == menu.length() - 1 || index == 0 || numberOfDash != 1) {
-            throw new IllegalArgumentException("입력 형식에 맞춰서 입력해주세요");
+            throw new IllegalArgumentException(Error.MENU.getMessage());
         }
     }
 
 
-    private void validateParsible(String input) {
+    private void validateParsible(String input, String errorMessage) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("당월에 해당하는 날짜만 입력해 주세요");
+            throw new IllegalArgumentException(errorMessage);
         }
     }
 
 
-    private void validateNullOrEmpty(String input) {
+    private void validateNullOrEmpty(String input, String errorMessage) {
         if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("입력을 해주세요");
+            throw new IllegalArgumentException(errorMessage);
         }
     }
 }
