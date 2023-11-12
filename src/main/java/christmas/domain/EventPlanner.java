dfeea66e77d56451c05169d82298f5d5bbbfee9e
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-13 02:01:06 +0900

    refactor(EventPlanner): 가독성 향상, 매직넘버 상수화 적용

--- src/main/java/christmas/domain/EventPlanner.java
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -2,19 +2,17 @@ package christmas.domain;
 
 import christmas.constants.Policy;
 import christmas.constants.messages.Error;
+import christmas.constants.messages.Notification;
 import christmas.domain.startegies.DesignatedDayStrategy;
 import christmas.domain.startegies.HolidayStrategy;
 import christmas.domain.startegies.SpecialStrategy;
 import christmas.domain.startegies.WeekDayStrategy;
 import christmas.dto.EventPlannerDto;
-import christmas.dto.MenusDto;
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 public class EventPlanner {
 
@@ -27,46 +25,46 @@ public class EventPlanner {
         this.menus = menus;
     }
 
-
-    private MenusDto getMenusDto() {
-        return menus.toDto();
+    public static EventPlanner of(Integer date, Menus menus) {
+        return new EventPlanner(date, menus);
     }
 
     private List<Map.Entry<String, Integer>> getDiscount() {
-        if (menus.totalPrice() < 10000) {
+        if (menus.totalPrice() < Policy.MIN_COST_FOR_DISCOUNT.getValue()) {
             return null;
         }
         Discount discount = Discount.of(date, menus);
         List<Entry<String, Integer>> discountResult = discount.calculateByStrategies(new HolidayStrategy(),
-                new DesignatedDayStrategy(), new SpecialStrategy(),
-                new WeekDayStrategy());
+                new DesignatedDayStrategy(), new SpecialStrategy(), new WeekDayStrategy());
         if (isGift()) {
-            discountResult.add(Map.entry("증정 이벤트", 25000));
+            discountResult.add(Map.entry(Notification.GIFT_EVENT.getMessage(), Policy.CHAMPAGNE.getValue()));
         }
         return discountResult;
     }
 
     private boolean isGift() {
-        return menus.totalPrice() >= 120000;
+        return menus.totalPrice() >= Policy.MIN_COST_FOR_GIFT.getValue();
+    }
+
+    private Integer getGiftPrice() {
+        if (isGift()) {
+            return Policy.CHAMPAGNE.getValue();
+        }
+        return 0;
     }
 
     private String getBadge(Integer afterDiscount) {
-        if (afterDiscount >= 20000) {
-            return "산타";
+        if (afterDiscount >= Policy.SANTA.getValue()) {
+            return Notification.SANTA.getMessage();
         }
-        if (afterDiscount >= 10000) {
-            return "트리";
+        if (afterDiscount >= Policy.TREE.getValue()) {
+            return Notification.TREE.getMessage();
         }
-        if (afterDiscount >= 5000) {
-            return "별";
+        if (afterDiscount >= Policy.STAR.getValue()) {
+            return Notification.STAR.getMessage();
         }
-        return "없음";
+        return Notification.NONE.getMessage();
     }
-
-    private Integer getBeforeDiscount() {
-        return menus.totalPrice();
-    }
-
 
     private Integer getTotalDiscount(List<Map.Entry<String, Integer>> discount) {
         if (discount == null) {
@@ -77,13 +75,8 @@ public class EventPlanner {
 
     public EventPlannerDto toDto() {
         int discounted = getTotalDiscount(getDiscount());
-        // 총혜택 금액 = 할인 금액의 합계 + 증정 메뉴의 가격
-        int totalBenefit = 0;
-        if (isGift()) {
-            totalBenefit += 25000;
-        }
-        int afterDiscount = menus.totalPrice() - discounted;
-        totalBenefit += discounted;
+        int afterDiscount = menus.totalPrice() - discounted; // 할인 후 예상 결제 금액 = 할인 전 총주문 금액 - 할인 금액
+        int totalBenefit = discounted + getGiftPrice(); // 총혜택 금액 = 할인 금액의 합계 + 증정 메뉴의 가격
         return new EventPlannerDto(date, menus.toDto(), menus.totalPrice(), isGift(), getDiscount(), totalBenefit,
                 afterDiscount, getBadge(afterDiscount));
     }
@@ -94,13 +87,5 @@ public class EventPlanner {
         } catch (DateTimeException e) {
             throw new IllegalArgumentException(Error.DATE.getMessage());
         }
-    }
-
-    public static void validateBeverage(Menus menus) {
-        Set<String> categories = new HashSet<>();
-    }
-
-    public static EventPlanner of(Integer date, Menus menus) {
-        return new EventPlanner(date, menus);
     }
 }
