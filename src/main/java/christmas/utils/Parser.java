1aa90cdadd1d1543f9451afcb4cb176b3307a9b2
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-12 19:41:26 +0900

    feat(Parser): 중복된 이름이 있으면 예외를 발생시키는 기능

--- src/main/java/christmas/utils/Parser.java
+++ src/main/java/christmas/utils/Parser.java
@@ -1,17 +1,24 @@
-package christmas.Utils;
+package christmas.utils;
 
+import christmas.constants.messages.Error;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Parser {
-
     public static List<String> parseMenuInput(String input) {
         List<String> parsedInput = Arrays.stream(input.split(",")).toList();
         List<String> result = new ArrayList<>();
+        Set<String> names = new HashSet<>();
         for (String name : parsedInput) {
             List<String> nameAndQuantity = Arrays.stream(name.split("-")).toList();
+            nameAndQuantity = nameAndQuantity.stream().map(String::trim).toList();
             validate(nameAndQuantity);
+            if (!names.add(nameAndQuantity.get(0))) {
+                throw new IllegalArgumentException(Error.MENU.getMessage());
+            }
             addNameToResultByQuantity(result, nameAndQuantity);
         }
         return result;
@@ -21,37 +28,36 @@ public class Parser {
         isNullOrEmpty(nameAndQuantity);
         isParsedToNameAndQuantity(nameAndQuantity);
         isQuantityParsible(nameAndQuantity);
-        Integer quantity = Integer.parseInt(nameAndQuantity.get(1));
-        isQuantityInRange(quantity);
+        isQuantityInRange(nameAndQuantity);
     }
 
     private static void isNullOrEmpty(List<String> nameAndQuantity) {
         if (nameAndQuantity == null || nameAndQuantity.isEmpty()) {
-            throw new IllegalArgumentException("input format error");
+            throw new IllegalArgumentException(Error.MENU.getMessage());
         }
     }
 
     private static void isParsedToNameAndQuantity(List<String> nameAndQuantity) {
         if (nameAndQuantity.size() != 2) {
-            throw new IllegalArgumentException("FORMAT ERROR");
+            throw new IllegalArgumentException(Error.MENU.getMessage());
         }
     }
 
     private static void isQuantityParsible(List<String> nameAndQuantity) {
         try {
-            String quantity = nameAndQuantity.get(1);
-            Integer.parseInt(quantity);
+            Integer.parseInt(nameAndQuantity.get(1));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("QUANTITY ERROR");
+            throw new IllegalArgumentException(Error.MENU.getMessage());
         }
     }
 
-    private static void isQuantityInRange(Integer quantity) {
+    private static void isQuantityInRange(List<String> nameAndQuantity) {
+        int quantity = Integer.parseInt(nameAndQuantity.get(1));
         if (quantity > 20) {
-            throw new IllegalArgumentException("QUANTITY TOO LARGE");
+            throw new IllegalArgumentException(Error.MENU.getMessage());
         }
         if (quantity == 0) {
-            throw new IllegalArgumentException("QUANTITY AT LEAST ONE");
+            throw new IllegalArgumentException(Error.MENU.getMessage());
         }
     }
 
